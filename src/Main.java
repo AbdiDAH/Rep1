e457e0465ab3ce1c4f6bc70517d706b6dda478c2
abirahmandahir <abdi-barca@hotmail.com>
2022-10-26 13:13:16 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Jag skrev precis ut den här texten!");
+
+        System.out.println(1337+1337);
+
+
+
+        int number = 0 ;
+
+        do {
+            System.out.println(number);
+            number++;
+        } while (number < 10);
+
+        while (number > 10 );
+        {
+            System.out.println("Från while: " + number);
+
+
+        }
+
+        String text = " Jag heter abdi ";
+        int age = 22;
+        String Text2 = " år gammal! ";
+
+        System.out.println(text + age + Text2);
+
+        double tal = (175335*2);
+        double tal2 = (178-500)/6.0;
+
+        System.out.println(tal+tal2);
+
+
+
+
+
+
+
+    }
+
+}
